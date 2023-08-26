fbd8da34f14a329940b5b8d426ccda9ca70442fc
Serhii Berdianskyi <zeferot94@gmail.com>
2023-08-26 18:25:03 +0300

    Created packages and modules, and implemented the cinema_visit function using imported modules

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list[dict],
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    CinemaBar()
+    for customer in customers:
+        CinemaBar.sell_product(customer["food"],
+                               Customer(customer["name"],
+                                        customer["food"]))
+    CinemaHall(hall_number).movie_session(movie_name=movie,
+                                          customers=[Customer(customer["name"],
+                                                              customer["food"])
+                                                     for customer in customers
+                                                     ],
+                                          cleaning_staff=Cleaner(cleaner))
+    Cleaner(cleaner)
