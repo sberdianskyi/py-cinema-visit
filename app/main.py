09466db19780d3d847f72b79e0fcc12887b98b2c
Serhii Berdianskyi <zeferot94@gmail.com>
2023-08-27 19:46:11 +0300

    Removed CinemaBar() and Cleaner(cleaner)

--- app/main.py
+++ app/main.py
@@ -8,13 +8,12 @@ def cinema_visit(customers: list[dict],
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
+    customers_list = []
     for customer in customers:
+        customer_object = Customer(customer["name"], customer["food"])
+        customers_list.append(customer_object)
         CinemaBar.sell_product(customer["food"],
-                               Customer(customer["name"],
-                                        customer["food"]))
+                               customer_object)
     CinemaHall(hall_number).movie_session(movie_name=movie,
-                                          customers=[Customer(customer["name"],
-                                                              customer["food"])
-                                                     for customer in customers
-                                                     ],
+                                          customers=customers_list,
                                           cleaning_staff=Cleaner(cleaner))
